b313796dfc14b1a8a858600f28312e75bab5b45e
KH <KH@DESKTOP-J093ICA>
2025-10-15 17:50:02 +0900

    12

--- boot_251015/src/main/java/com/boot/ExamController.java
+++ boot_251015/src/main/java/com/boot/ExamController.java
@@ -11,7 +11,7 @@ public class ExamController {
 	@RequestMapping("/")
 //	public void home() {
 	public String home() {
-		log.info("hello boot777 222");
+		log.info("hello boot777 111 222");
 		
 		return "hello";
 	}

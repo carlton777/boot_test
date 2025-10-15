a86806e232d976370808e4fbbb63d27b70ad6ebf
carlton777 <sangkoo@naver.com>
2025-10-15 17:30:33 +0900

    init commit

---
+++ boot_251015/src/main/java/com/boot/Boot251015Application.java
@@ -0,0 +1,13 @@
+package com.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot251015Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot251015Application.class, args);
+	}
+
+}
